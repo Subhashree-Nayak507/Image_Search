4ca118004627209e63d73a4bfc56aee62f2d6a62
Subhashree-Nayak507 <kukisubhashree@gmail.com>
2025-05-10 12:40:14 +0530

    m

--- server/index.js
+++ server/index.js
@@ -13,11 +13,17 @@ const app = express();
 const __dirname = path.resolve();
 
 app.use(
-    cors({
-      origin: "http://localhost:5173",
-      credentials: true,
-    })
-  );
+  cors({
+    origin: [
+      'http://localhost:4000', 
+      'https://image-search-ebj9.onrender.com' 
+    ],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  })
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended :true}));
 app.use(cookieParser());
